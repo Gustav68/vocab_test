4de045a780e2d7a2dc6130e19842a275eccb10f5
Zubák <vladimir.zubak@siemens.com>
2023-11-23 17:30:51 +0100

    cold start

--- vocab_test.py
+++ vocab_test.py
@@ -1,5 +1,17 @@
+import random
+
+
 def main():
-  print("Hello and see, if it gets through")
+  test_dict = {}
+  path = "dict_1.txt"
+  file = open(path, "r")
+  dict = file.read()
+    
+  """for line in dict:
+      (key, val) = line.split(" : ")
+      test_dict[key] = val"""
+  print(dict)
+  file.close()
 
 if __name__ == "__main__":
   main()
