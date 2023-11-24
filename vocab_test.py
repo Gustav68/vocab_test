803b9e46e64b805e8d2a4b31c99c5eaf36641ab6
Zubák <vladimir.zubak@siemens.com>
2023-11-24 13:42:07 +0100

    dictionary opening

--- vocab_test.py
+++ vocab_test.py
@@ -1,17 +1,18 @@
 import random
 
 
+def open_dict(path):
+  dict_o = {} 
+  with open(path, "r", encoding='utf-8') as file:
+    for line in file:
+        key, val = line.strip().split(" : ")
+        dict_o[key] = val
+  return(dict_o)
+  
 def main():
-  test_dict = {}
-  path = "dict_1.txt"
-  file = open(path, "r")
-  dict = file.read()
-    
-  """for line in dict:
-      (key, val) = line.split(" : ")
-      test_dict[key] = val"""
-  print(dict)
-  file.close()
+  selected_dict = "dict_1.txt"
+  test_dict = open_dict(selected_dict)
+  print(test_dict)
 
 if __name__ == "__main__":
   main()
